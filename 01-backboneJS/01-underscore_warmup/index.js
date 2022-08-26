fbc141811db635e33891efbf483f2322fca5cbad
Caleb M Rank <calebmrank@gmail.com>
2022-08-26 10:55:48 -0600

    initial commit

---
+++ 01-backboneJS/01-underscore_warmup/index.js
@@ -0,0 +1,15 @@
+const _ = require('underscore');
+
+const myState = {
+    name: "Washington",
+    capital: "Olympia",
+    population: 7062000,
+    nickname: "The Evergreen State"
+};
+const businesses = [
+    { name: "Coding Dojo", city:  "Bellevue" },
+    { name: "Facebook", city: "Mountain View" },
+    { name: "Microsoft", city: "Redmond" },
+    { name: "Expedia", city: "Bellevue" }
+];
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Coding_Dojo-JS_Front-End_Frameworks
