312bb7f2e99860d3f3d60588b63d7f66a52e3cbb
Caleb M Rank <calebmrank@gmail.com>
2022-09-14 09:47:41 -0600

    commit 09/14/2022

---
+++ 03-angularJS/10-routing/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { routing } from './app.routes';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NotesComponent } from './notes/notes.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        TasksComponent,
+        NotesComponent
+    ],
+    imports: [
+        routing,
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
